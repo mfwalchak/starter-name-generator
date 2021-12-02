b9afa0385d1a3b494f8016400cbd662d509a894b
Mike Walchak <mfwalchak@gmail.com>
2021-12-01 20:30:17 -0700

    Fix fetch URL

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
+    fetch('https://warm-river-06379-client.herokuapp.com/generate')
       .then((res) => res.json())
       .then((nme) => setName([nme]))
   }, [])
